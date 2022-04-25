ed633329ed65808bdad6ad02e38986e1454317b9
anukulmoon96 <98376820+anukulmoon96@users.noreply.github.com>
2022-04-25 13:33:35 +0530

    Added Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import ResponsiveAppBar from "./MaterialUiComponents/AppBar";
+import SideBar from "./components/Sidebar/SideBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Messages from "./pages/Messages";
+import FileManager from "./pages/FileManager";
+import Analytics from "./pages/Analytics";
+import Order from "./pages/Order";
+import Saved from "./pages/Saved";
+import Setting from "./pages/Setting";
 function App() {
 	return (
 		<div className="App">
 			<ResponsiveAppBar />
-			<div className="newApp"></div>
+			<Router>
+      <SideBar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/file-manager" element={<FileManager />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/settings" element={<Setting />} />
+
+          <Route path="*" element={<> not found</>} />
+        </Routes>
+      </SideBar>
+    </Router>
 		</div>
 	);
 }
